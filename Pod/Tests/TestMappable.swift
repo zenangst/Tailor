a38e0adfab61b2c229080b11ff516549878939fe
Vadym Markov <markov.vadym@gmail.com>
2015-09-11 21:50:03 +0200

    Add tests

--- Pod/Tests/TestMappable.swift
+++ Pod/Tests/TestMappable.swift
@@ -7,26 +7,33 @@ class TestMappable: XCTestCase {
     var expectedStruct = TestPersonStruct([:])
     expectedStruct.firstName = "Taylor"
     expectedStruct.lastName = "Swift"
+    expectedStruct.sex = .Female
 
     let testStruct = TestPersonStruct([
       "firstName" : "Taylor",
-      "lastName" : "Swift"
+      "lastName" : "Swift",
+      "sex": "female"
       ])
 
-    XCTAssertEqual(testStruct, expectedStruct)
+    XCTAssertEqual(testStruct.firstName, expectedStruct.firstName)
+    XCTAssertEqual(testStruct.lastName, expectedStruct.lastName)
+    XCTAssertEqual(testStruct.sex, expectedStruct.sex)
   }
 
   func testMappableClass() {
     let expectedClass = TestPersonClass([:])
     expectedClass.firstName = "Taylor"
     expectedClass.lastName = "Swift"
+    expectedClass.sex = .Female
 
     let testClass = TestPersonClass([
       "firstName" : "Taylor",
-      "lastName" : "Swift"
+      "lastName" : "Swift",
+      "sex": "female"
       ])
 
     XCTAssertEqual(testClass.firstName, expectedClass.firstName)
     XCTAssertEqual(testClass.lastName, expectedClass.lastName)
+    XCTAssertEqual(testClass.sex, expectedClass.sex)
   }
 }

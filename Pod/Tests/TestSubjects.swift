20e0546781c7a88ec1f24c631811d8bb6ec3bfb6
Vadym Markov <markov.vadym@gmail.com>
2015-09-11 23:03:09 +0200

    Add date tests

--- Pod/Tests/TestSubjects.swift
+++ Pod/Tests/TestSubjects.swift
@@ -12,6 +12,7 @@ class TestPersonClass: NSObject, Inspectable, Mappable {
   var firstName: String = ""
   var lastName: String? = ""
   var sex: Sex = .Unspecified
+  var birthDate: NSDate?
   
   required convenience init(_ map: [String : AnyObject]) {
     self.init()
@@ -22,6 +23,16 @@ class TestPersonClass: NSObject, Inspectable, Mappable {
       var result: Sex?
       if let value = value {
         result = Sex(rawValue: value)
+      }
+      return result
+    }
+
+    birthDate <- map.propertyWithTransform("birth_date") { (value: String?) -> NSDate? in
+      var result: NSDate?
+      if let value = value {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        result = dateFormatter.dateFromString(value)
       }
       return result
     }
@@ -36,7 +47,8 @@ class TestPersonClass: NSObject, Inspectable, Mappable {
 struct TestPersonStruct: Inspectable, Equatable {
   var firstName: String = ""
   var lastName: String? = ""
-  var sex: Sex = .Female
+  var sex: Sex?
+  var birthDate = NSDate(timeIntervalSince1970: 1)
 
   init(_ map: [String : AnyObject]) {
     firstName <- map.property("firstName")
@@ -46,6 +58,16 @@ struct TestPersonStruct: Inspectable, Equatable {
       var result: Sex?
       if let value = value {
         result = Sex(rawValue: value)
+      }
+      return result
+    }
+
+    birthDate <- map.propertyWithTransform("birth_date") { (value: String?) -> NSDate? in
+      var result: NSDate?
+      if let value = value {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        result = dateFormatter.dateFromString(value)
       }
       return result
     }

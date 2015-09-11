9d5d202a1fb3a2c39b20c8d6c012653700d80845
Vadym Markov <markov.vadym@gmail.com>
2015-09-11 21:49:47 +0200

    Add test data

--- Pod/Tests/TestSubjects.swift
+++ Pod/Tests/TestSubjects.swift
@@ -1,14 +1,30 @@
 import Foundation
 import Tailor
 
+enum Sex: String {
+  case Unspecified = "unspecified"
+  case Male = "male"
+  case Female = "female"
+}
+
 class TestPersonClass: NSObject, Inspectable, Mappable {
+
   var firstName: String = ""
   var lastName: String? = ""
+  var sex: Sex = .Unspecified
   
   required convenience init(_ map: [String : AnyObject]) {
     self.init()
     firstName <- map.property("firstName")
     lastName  <- map.property("lastName")
+
+    sex <- map.propertyWithTransform("sex") { (value: String?) -> Sex? in
+      var result: Sex?
+      if let value = value {
+        result = Sex(rawValue: value)
+      }
+      return result
+    }
   }
 
   func mapping(map: [String : AnyObject]) {
@@ -20,10 +36,19 @@ class TestPersonClass: NSObject, Inspectable, Mappable {
 struct TestPersonStruct: Inspectable, Equatable {
   var firstName: String = ""
   var lastName: String? = ""
-  
+  var sex: Sex = .Female
+
   init(_ map: [String : AnyObject]) {
     firstName <- map.property("firstName")
     lastName  <- map.property("lastName")
+
+    sex <- map.propertyWithTransform("sex") { (value: String?) -> Sex? in
+      var result: Sex?
+      if let value = value {
+        result = Sex(rawValue: value)
+      }
+      return result
+    }
   }
 }
 

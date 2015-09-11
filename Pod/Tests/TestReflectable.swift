18132165be153a308ddd7e335ff5b74035ef0e8a
Vadym Markov <markov.vadym@gmail.com>
2015-09-11 22:48:19 +0200

    Update tests

--- Pod/Tests/TestReflectable.swift
+++ Pod/Tests/TestReflectable.swift
@@ -6,23 +6,36 @@ class TestReflectable: XCTestCase {
   func testReflectionOnClass() {
     let firstName = "Taylor"
     let lastName = "Swift"
+    let sex = Sex.Female
+
     let person = TestPersonClass()
     person.firstName = firstName
     person.lastName = lastName
+    person.sex = sex
 
     XCTAssertEqual(firstName, person.firstName)
-    XCTAssertEqual(firstName, person.property("firstName") as? String)
+    XCTAssertEqual(firstName, person.property("firstName"))
+    XCTAssertEqual(lastName, person.lastName)
+    XCTAssertEqual(lastName, person.property("lastName"))
+    XCTAssertEqual(sex, person.sex)
+    XCTAssertEqual(sex, person.property("sex"))
   }
 
   func testReflectionOnStruct() {
     let firstName = "Taylor"
     let lastName = "Swift"
+    let sex = Sex.Female
+
     var person = TestPersonStruct([:])
     person.firstName = firstName
     person.lastName = lastName
+    person.sex = sex
 
     XCTAssertEqual(firstName, person.firstName)
-    XCTAssertEqual(firstName, person.property("firstName") as? String)
+    XCTAssertEqual(firstName, person.property("firstName"))
+    XCTAssertEqual(lastName, person.lastName)
+    XCTAssertEqual(lastName, person.property("lastName"))
+    XCTAssertEqual(sex, person.sex)
+    XCTAssertEqual(sex, person.property("sex"))
   }
-
 }

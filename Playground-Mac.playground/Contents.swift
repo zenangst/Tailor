5356a8bb279223c4dd5e9263558e211ac6c3ae2d
Christoffer Winterkvist <christoffer@hyper.no>
2016-02-15 20:28:30 +0100

    Improve playground to include new value extension

--- Playground-Mac.playground/Contents.swift
+++ Playground-Mac.playground/Contents.swift
@@ -6,10 +6,12 @@ import Tailor
 struct Person: Inspectable, Mappable, Equatable {
   var firstName: String = ""
   var lastName: String? = ""
+  var age: Int = 0
 
   init(_ map: [String : AnyObject]) {
     firstName <- map.property("firstName")
     lastName  <- map.property("lastName")
+    age       <- map.property("age")
   }
 }
 
@@ -17,6 +19,8 @@ func ==(lhs: Person, rhs: Person) -> Bool {
   return lhs.firstName == rhs.firstName && lhs.lastName == rhs.lastName
 }
 
-let taylor = Person(["firstName" : "Taylor", "lastName" : "Swift"])
-print(taylor.firstName)
-print(taylor.lastName)
+let taylor = Person(["firstName" : "Taylor", "lastName" : "Swift", "age" : 27])
+
+taylor.value("firstName", String.self)
+taylor.value("lastName", OptionalString.self)
+taylor.value("age", Int.self)

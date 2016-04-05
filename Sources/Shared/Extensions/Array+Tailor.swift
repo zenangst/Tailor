08fef09fbd2194e5e6d6a3c3ff0e826bce3e8705
Vadym Markov <markov.vadym@gmail.com>
2016-04-05 14:19:48 +0200

    Support objects for SafeMappable

--- Sources/Shared/Extensions/Array+Tailor.swift
+++ Sources/Shared/Extensions/Array+Tailor.swift
@@ -24,7 +24,30 @@ public extension Array {
 
     return objects
   }
- 
+
+  /**
+   - Parameter name: String
+   - Returns: A mappable object array, otherwise it returns nil
+   */
+  func objects<T : SafeMappable>(name: String? = nil) throws -> [T] {
+    var objects = [T]()
+
+    if let name = name {
+      for dictionary in self {
+        guard let dictionary = dictionary as? JSONDictionary,
+          value = dictionary[name] as? JSONDictionary else { continue }
+        objects.append(try T(value))
+      }
+    } else {
+      for dictionary in self {
+        guard let dictionary = dictionary as? JSONDictionary else { continue }
+        objects.append(try T(dictionary))
+      }
+    }
+
+    return objects
+  }
+
   /**
    - Parameter name: The index
    - Returns: A child dictionary at that index, otherwise it returns nil
@@ -32,10 +55,10 @@ public extension Array {
   func dictionary(index: Int) -> JSONDictionary? {
     guard index < self.count, let value = self[index] as? JSONDictionary
       else { return nil }
-    
+
     return value
   }
- 
+
   /**
    - Parameter name: The index
    - Returns: A child array at that index, otherwise it returns nil
@@ -43,7 +66,7 @@ public extension Array {
   func array(index: Int) -> JSONArray? {
     guard index < self.count, let value = self[index] as? JSONArray
       else { return nil }
-    
+
     return value
   }
 }

ad1f9fc7407864d8e215c5de3d850e0e727638ae
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-04 11:19:10 +0200

    Remove whitespace

--- TailorTests/Shared/TestAccessible.swift
+++ TailorTests/Shared/TestAccessible.swift
@@ -4,17 +4,17 @@ import Sugar
 
 struct Club {
   var people: [Person] = []
-  
+
   init(_ map: JSONDictionary) {
     people <- map.dictionary("detail")?.relations("people")
   }
 }
 
 struct Person: Mappable {
-  
+
   var firstName: String? = ""
   var lastName: String? = ""
-  
+
   init(_ map: JSONDictionary) {
     firstName <- map.property("first_name")
     lastName  <- map.property("last_name")
@@ -23,7 +23,7 @@ struct Person: Mappable {
 
 class TestAccessible: XCTestCase {
   func testAccessible() {
-    let json = [
+    let json: [String : AnyObject] = [
       "school": [
         "name": "Hyper",
         "clubs": [
@@ -60,7 +60,7 @@ class TestAccessible: XCTestCase {
         ]
       ]
     ]
-    
+
     XCTAssertEqual(json.path("school.clubs.0.detail.name"), "DC")
     XCTAssertEqual(json.path("school.clubs.0.detail.people.0.first_name"), "Clark")
     XCTAssertEqual(json.path("school.clubs.0.detail.people")!, [
@@ -73,16 +73,17 @@ class TestAccessible: XCTestCase {
         "last_name": "Wayne"
       ]
       ])
+
     if let marvelClubJSON = json.dictionary("school")?.array("clubs")?.dictionary(1) {
       let club = Club(marvelClubJSON)
-      
+
       let tony = club.people[0]
-      
+
       XCTAssertEqual(tony.firstName, "Tony")
       XCTAssertEqual(tony.lastName, "Stark")
-     
+
       let hulk = club.people[1]
-      
+
       XCTAssertEqual(hulk.firstName, "Bruce")
       XCTAssertEqual(hulk.lastName, "Banner")
     }
